aa60e3b8aca5a270ba39192ff72fa4523369a66a
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-07-08 16:24:33 +0100

    Update ybaspawnfixer.lua

--- ybaspawnfixer.lua
+++ ybaspawnfixer.lua
@@ -4,6 +4,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Tobias020108Back/YBA-
 local gui = game:GetService("CoreGui")["YBA Sucks Ass"]
 
 game:GetService("RunService").RenderStepped:Connect(function(step)
+        if not game:GetService("CoreGui")["YBA Sucks Ass"] then return end
         if game:GetService("CoreGui")["YBA Sucks Ass"] then
             gui:Destroy()
         end
