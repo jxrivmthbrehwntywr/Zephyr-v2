df5e89a9e1fd8a2b69de9df234551278c34d10fd
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-07-08 16:28:42 +0100

    Update ybaspawnfixer.lua

--- ybaspawnfixer.lua
+++ ybaspawnfixer.lua
@@ -6,6 +6,6 @@ local gui = game:GetService("CoreGui")["YBA Sucks Ass"]
 game:GetService("RunService").RenderStepped:Connect(function(step)
         if not game:GetService("CoreGui")["YBA Sucks Ass"] then return end
         if game:GetService("CoreGui")["YBA Sucks Ass"] then
-            gui:Destroy()
+            game:GetService("CoreGui")["YBA Sucks Ass"].Main.Visible = false
         end
 end)
