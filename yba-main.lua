fd788614697640c93c0650ec62237c0be20ab8ec
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-07-09 15:15:28 +0100

    Update yba-main.lua

--- yba-main.lua
+++ yba-main.lua
@@ -86,11 +86,11 @@ local itemFarm = coroutine.wrap(function()
 						local clickdetector = v.Parent.ClickDetector
 
 						playerRoot.CFrame = mesh.CFrame
-						wait(0.25)
+						wait(0.35)
 						fireclickdetector(clickdetector)
-						wait(0.25)
+						wait(0.35)
 					until not game:GetService("Workspace")["Item_Spawns"].Items:FindFirstChild(v)
-				
+				end
 			end
 		end
 	end
