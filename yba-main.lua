7ad8a5641dcac3ec4966749a3effd7267a686ebd
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-07-09 14:42:55 +0100

    Update yba-main.lua

--- yba-main.lua
+++ yba-main.lua
@@ -69,27 +69,29 @@ end)
 
 
 local itemFarm = coroutine.wrap(function()
-	while wait() do
+	while wait() do	
 		if getgenv().itemfarmvalue then
 			local playerRoot = game.Players.LocalPlayer.Character.HumanoidRootPart
-					for i, v in pairs(game:GetService("Workspace"):GetDescendants()) do
-						if v.Name =="MeshPart" and v.Parent and v.Parent.Name == "Model" and getgenv().itemfarmvalue then
-						repeat 
-							local mesh = v 
-							local model = v.Parent
-							local clickdetector = v.Parent.ClickDetector
+			
+			for i, v in pairs(game:GetService("Workspace"):GetDescendants()) do
+				if v.Name =="MeshPart" and v.Parent and v.Parent.Name == "Model" and getgenv().itemfarmvalue then
+				repeat 
+					local mesh = v 
+					local model = v.Parent
+					local clickdetector = v.Parent.ClickDetector
 
-							playerRoot.CFrame = mesh.CFrame
-							wait(0.32)
-							fireclickdetector(clickdetector)
-							wait(0.32)
-						until not game:GetService("Workspace")["Item_Spawns"].Items:FindFirstChild(v)
-					end
-				end
+					playerRoot.CFrame = mesh.CFrame
+					wait(0.32)
+					fireclickdetector(clickdetector)
+					wait(0.32)
+				until not game:GetService("Workspace")["Item_Spawns"].Items:FindFirstChild(v)
 			end
+				
 		end
 	end
 end)
+
+
 
 itemFarm()
 
