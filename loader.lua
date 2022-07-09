57d1bb1a2f3c59d382de75b436095e2b409cdc66
jxrivmthbrehwntywr <108086564+jxrivmthbrehwntywr@users.noreply.github.com>
2022-07-09 07:12:23 +0100

    Update and rename loader to loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+
+if game.PlaceId == 3652625463 then --Lifting Simulator
+  loadstring(game:HttpGet("https://github.com/jxrivmthbrehwntywr/Zephyr-v2/blob/main/liftingsim-main.lua"))()
+else if game.PlaceId == 2809202155 then --YBA
+loadstring(game:HttpGet("https://github.com/jxrivmthbrehwntywr/Zephyr-v2/blob/main/yba-main.lua"))()
+end
+  
+  --loader loadstring(game:HttpGet("https://github.com/jxrivmthbrehwntywr/Zephyr-v2/edit/main/loader.lua"))()
